189012bd1cc8ab20546adf6cbfd8811b8e366701
Luc Thuy <nguyenducminh01122002@gmail.com>
2023-01-10 11:05:12 +0700

    update admin view

---
+++ QuanLyNhanKhau/src/main/java/cnpm/QuanLyNhanKhau/controller/ControllerMainAdmin.java
@@ -0,0 +1,91 @@
+package cnpm.QuanLyNhanKhau.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.util.Duration;
+
+public class ControllerMainAdmin implements Initializable {
+
+	@FXML
+	private Button buttonQuanLy;
+	@FXML
+	private Button buttonNhanKhau;
+	@FXML
+	private Button buttonHoKhau;
+	@FXML
+	private Button buttonTamTruTamVang;
+
+	@FXML
+	private Parent viewQuanLy;
+	@FXML
+	private Parent viewNhanKhau;
+	@FXML
+	private Parent viewHoKhau;
+	@FXML
+	private Parent viewTamTruTamVang;
+	
+	private Button currentButton;
+	private Parent currentView;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {		
+		currentButton = buttonQuanLy;
+		currentView = viewQuanLy;
+		
+		buttonQuanLy.setTranslateX(-10);
+		buttonNhanKhau.setTranslateX(-10);
+		buttonHoKhau.setTranslateX(-10);
+		buttonTamTruTamVang.setTranslateX(-10);
+		
+		setViewQuanLy();
+	}
+	
+	@FXML
+	public void setViewQuanLy() {
+		setView(viewQuanLy, buttonQuanLy);
+	}
+	
+	@FXML
+	public void setViewNhanKhau() {
+		setView(viewNhanKhau, buttonNhanKhau);	
+	}
+	
+	@FXML
+	public void setViewHoKhau() {
+		setView(viewHoKhau, buttonHoKhau);
+	}
+	
+	@FXML
+	public void setViewTamTruTamVang() {
+		setView(viewTamTruTamVang, buttonTamTruTamVang);
+	}
+
+	private void setView(Parent view, Button button) {
+		TranslateTransition translateTransition;
+		
+		currentView.setVisible(false);
+		currentButton.getStyleClass().removeAll("active");
+		translateTransition = new TranslateTransition(Duration.millis(500), currentButton);
+		translateTransition.setFromX(currentButton.getTranslateX());
+		translateTransition.setToX(-10);
+		translateTransition.play();
+		
+		view.setVisible(true);
+		view.toFront();
+		view.requestFocus();
+		button.getStyleClass().add("active");
+		translateTransition = new TranslateTransition(Duration.millis(500), button);
+		translateTransition.setFromX(button.getTranslateX());
+		translateTransition.setToX(0);
+		translateTransition.play();
+		
+		currentView = view;
+		currentButton = button;
+	}
+}
---
+++ QuanLyNhanKhau/src/main/java/cnpm/QuanLyNhanKhau/controller/ControllerQuanLy.java
@@ -0,0 +1,115 @@
+package cnpm.QuanLyNhanKhau.controller;
+
+import java.net.URL;
+import java.sql.Date;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import cnpm.QuanLyNhanKhau.Connector;
+import cnpm.QuanLyNhanKhau.model.ModelUser;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+
+public class ControllerQuanLy implements Initializable{
+
+	@FXML
+	private TableView<ModelUser> tableviewUser;
+	@FXML
+	private TableColumn<ModelUser, String> tablecolumnTenNguoiDung;
+	@FXML
+	private TableColumn<ModelUser, Date> tablecolumnTaiKhoan;
+	@FXML
+	private TableColumn<ModelUser, String> tablecolumnMatKhau;
+	
+	@FXML
+	private TableView<ModelUser> tableviewUserNV;
+	@FXML
+	private TableColumn<ModelUser, String> tablecolumnTenNguoiDungNV;
+	@FXML
+	private TableColumn<ModelUser, Date> tablecolumnTaiKhoanNV;
+	@FXML
+	private TableColumn<ModelUser, String> tablecolumnMatKhauNV;
+	
+	@FXML
+	private Button buttonPheDuyet;
+	@FXML
+	private Button buttonTuChoi;
+	
+	private ModelUser user;
+	private ModelUser userNV;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		tablecolumnTenNguoiDung.setCellValueFactory(new PropertyValueFactory<>("tenNguoiDung"));
+		tablecolumnTaiKhoan.setCellValueFactory(new PropertyValueFactory<>("taiKhoan"));
+		tablecolumnMatKhau.setCellValueFactory(new PropertyValueFactory<>("matKhau"));
+		
+		tableviewUser.setRowFactory( val -> {
+			TableRow<ModelUser> row = new TableRow<>();
+			row.setOnMouseClicked(e -> {
+					user = row.getItem();
+			});
+			return row;
+		});
+		
+		tablecolumnTenNguoiDungNV.setCellValueFactory(new PropertyValueFactory<>("tenNguoiDung"));
+		tablecolumnTaiKhoanNV.setCellValueFactory(new PropertyValueFactory<>("taiKhoan"));
+		tablecolumnMatKhauNV.setCellValueFactory(new PropertyValueFactory<>("matKhau"));
+		
+		tableviewUserNV.setRowFactory( val -> {
+			TableRow<ModelUser> row = new TableRow<>();
+			row.setOnMouseClicked(e -> {
+					userNV = row.getItem();
+			});
+			return row;
+		});
+		
+		refreshTableViewUser();
+		refreshTableViewUserNV();
+	}
+	
+	private void refreshTableViewUser() {
+		tableviewUser.getItems().clear();
+		
+		List<ModelUser> listUser = Connector.getAllUserByRole("DANG KY");
+		listUser.forEach(user -> {
+			tableviewUser.getItems().add(user);
+		});
+	}
+	
+	private void refreshTableViewUserNV() {
+		tableviewUserNV.getItems().clear();
+		
+		List<ModelUser> listUser = Connector.getAllUserByRole("NHAN VIEN");
+		listUser.forEach(user -> {
+			tableviewUserNV.getItems().add(user);
+		});
+	}
+	
+	@FXML
+	public void confirmUser() {
+		user.setRole("NHAN VIEN");
+		Connector.editUser(user);
+		
+		refreshTableViewUser();
+	}
+
+	@FXML
+	public void declineUser() {
+		Connector.deleteUser(user);
+		
+		refreshTableViewUser();
+	}
+	
+	@FXML
+	public void showDetailUser() {
+		
+	}
+	
+}
